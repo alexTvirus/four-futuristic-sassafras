afc5b13d2bf55a739d6a10035520d519a3522e48
Glitch (four-futuristic-sassafras) <none>
2022-06-04 10:54:35 +0000

    Import of alexTvirus/node-heroku-proxy

---
+++ git.sh
@@ -0,0 +1,10 @@
+
+git remote add origin https://github.com/alexTvirus/node-heroku-proxy.git
+
+git fetch origin main
+
+# Hard reset
+git reset --hard origin/main
+
+# Force pull
+git pull origin main --force
