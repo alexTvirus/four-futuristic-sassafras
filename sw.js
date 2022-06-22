471c37afc61a8799b372de66b6611d1b8e521a93
Glitch (four-futuristic-sassafras) <none>
2022-06-22 11:19:51 +0000

    🍿😂 Checkpoint ./sw.js:45532465/525

--- sw.js
+++ sw.js
@@ -7,22 +7,7 @@ var supportPreloading = false;
 self.addEventListener('fetch', function(event) {
 	const url = event.request.url;
    if (url.startsWith(self.location.origin)){
-      	 event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+      	return fetch(event.request);
    }else{
 	   event.respondWith(handleRequest(event.request)) // add your custom response
 
