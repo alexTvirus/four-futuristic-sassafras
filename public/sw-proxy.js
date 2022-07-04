7ad9b4ff992b08f7855484613b22d1d2506639d8
Glitch (four-futuristic-sassafras) <none>
2022-07-04 15:40:06 +0000

    💂🚪 Checkpoint ./public/sw-proxy.js:45532465/523

--- public/sw-proxy.js
+++ public/sw-proxy.js
@@ -8,7 +8,9 @@ self.addEventListener('fetch', function(event) {
 	const url = event.request.url;
    if (url.startsWith(self.location.origin)){
       	event.respondWith(fetch(event.request))
-   }else{
+   }else if (!url.toString().startsWith("https://www.pornhub.com")){
+	   event.respondWith(fetch(event.request))
+   }else {
 	   event.respondWith(handleRequest(event.request)) // add your custom response
 
    }
@@ -44,9 +46,7 @@ async function handleRequest(request) {
 
     let url = new URL(request.url);
     let hostname=url.hostname
-    if (url.toString().startsWith("https://www.pornhub.com")){
-        url.hostname = 'infinityproxy.tk'
-    }
+    url.hostname = 'infinityproxy.tk'
     console.log("url "+url.toString());
     url.protocol ='https:'
     let req = new Request(url.toString(),request);
@@ -54,7 +54,7 @@ async function handleRequest(request) {
     req.headers.set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36')
     const response = await fetch(req);
     const newResponse = new Response(response.body, response);
-    //newResponse.headers.append('cac',url.toString())
+    newResponse.headers.append('cac',url.toString())
     newResponse.headers.append('Access-Control-Allow-Origin','*')
 
     return newResponse
